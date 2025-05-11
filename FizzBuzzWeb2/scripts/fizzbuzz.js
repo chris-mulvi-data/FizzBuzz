0def458d81229a5f364bb7dc7522aefe3d909501
Chris Mulvihill <mulvihill.chris@outlook.com>
2025-05-11 13:45:20 -0600

    updates from lapytop

--- FizzBuzzWeb2/scripts/fizzbuzz.js
+++ FizzBuzzWeb2/scripts/fizzbuzz.js
@@ -6,13 +6,27 @@ $(document).ready(function() {
 
     $("#start-button").on("click", function() {
         console.log("start buttong clicked")
-        $("#game-input-dialog")[0].showModal()
+        let data = get_inputs()
+        fizzBuzz(data)
     })
-
-    $("#push-me").on("click", function() {
-
-        $("#game-input-dialog")[0].close()
-    })
-
 });
 
+function get_inputs() {
+    data = {}
+    inputs = $("#controls").find("input")
+
+        .each(function() {
+            if ($(this).attr("type") == "number") {
+
+                data[$(this).attr("id")] = parseInt($(this).val())
+            } else {
+                data[$(this).attr("id")] = $(this).val()
+            }
+        })
+
+    return data
+}
+
+function fizzBuzz(data) {
+    console.log(data)
+}
