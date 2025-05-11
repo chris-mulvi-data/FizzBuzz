0f36c295dcd421fbbd19859b3f108f94d1446899
Chris Mulvihill <mulvihill.chris@outlook.com>
2025-05-11 11:30:34 -0600

    building modal

--- FizzBuzzWeb2/scripts/fizzbuzz.js
+++ FizzBuzzWeb2/scripts/fizzbuzz.js
@@ -6,6 +6,13 @@ $(document).ready(function() {
 
     $("#start-button").on("click", function() {
         console.log("start buttong clicked")
+        $("#game-input-dialog")[0].showModal()
+    })
+
+    $("#push-me").on("click", function() {
+
+        $("#game-input-dialog")[0].close()
     })
 
 });
+
